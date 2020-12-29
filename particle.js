01e0e3881a10cf50cea79667e8eb3ae7a03200ae
AgentAction <72904185+AgentAction@users.noreply.github.com>
2020-12-29 20:18:44 +0530

    Update particle.js

--- particle.js
+++ particle.js
@@ -1,11 +1,11 @@
 class Particle{
     constructor(x,y,r){
      var opt={
-         isStatic:false,
-         restitution:0.3
+         "restitution":0.3,
+         "friction":0.1
      }
      this.r = r;
-     this.body = Bodies.circle(x,y,r);
+     this.body = Bodies.circle(x,y,r,opt);
      this.color = color(random(0,255),random(0,255),random(0,255));
      World.add(world,this.body);
 
